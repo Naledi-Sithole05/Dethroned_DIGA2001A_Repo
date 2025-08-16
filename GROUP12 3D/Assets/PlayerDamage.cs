6037c9939ca79e757b5f86a1e5902ca185101cb5
Naledi-Sithole05 <justnalz123@gmail.com>
2025-08-17 00:16:19 +0200

    player damage
    added player damage script, wrecking balls, jump distance

---
+++ GROUP12 3D/Assets/PlayerDamage.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public int maxHealth = 5;
+    private int currentHealth;
+
+    [Header("References")]
+    public Healthbar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+        healthbar.SetHealth(currentHealth);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Trap or Beam deals damage
+        if (collision.gameObject.CompareTag("Trap") || collision.gameObject.CompareTag("Beam"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        healthbar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
