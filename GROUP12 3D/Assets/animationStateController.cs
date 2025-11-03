1b1db22079f053a1209c65ad43f2b7e3ae2da5f1
Atleweezy <atlegangnthite@gmail.com>
2025-11-03 16:14:53 +0200

    Setting up walking animation

---
+++ GROUP12 3D/Assets/animationStateController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+    }
+
+}
