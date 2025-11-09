489e8cf409ac181f4676f2381c1e834f69ad360e
Naledi-Sithole05 <justnalz123@gmail.com>
2025-11-09 14:17:12 +0200

    guard navigation added

--- GROUP12 3D/Assets/GuardAnimationStateContoller.cs
+++ GROUP12 3D/Assets/GuardAnimationStateContoller.cs
@@ -1,27 +1,44 @@
 using UnityEngine;
 
-public class GuardAnimationStateContoller : MonoBehaviour
+public class GuardAnimationStateController : MonoBehaviour
 {
-    Animator animator;
-    int isWalkingHash;
+    private Animator animator;
+    private int isWalkingHash;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+
+        // Ensure the parameter exists before trying to set it
+        if (animator.HasParameterOfType(isWalkingHash, AnimatorControllerParameterType.Bool))
+        {
+            animator.SetBool(isWalkingHash, true); // Guard will always walk
+        }
+        else
+        {
+            Debug.LogWarning(" Animator missing parameter 'isWalking' on " + gameObject.name);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        bool isWalking = animator.GetBool(isWalkingHash);
-        if (isWalking)
-        {
-            animator.SetBool(isWalkingHash, true);
-        }
-        if (!isWalking)
-        {
-            animator.SetBool(isWalkingHash, true);
-        }
+        // Optional: Add patrol or AI movement later here.
+        // The guard will stay in the walking animation continuously.
     }
 }
+
+//  Extension helper (you can leave this in the same file)
+public static class AnimatorExtensions
+{
+    public static bool HasParameterOfType(this Animator self, int hash, AnimatorControllerParameterType type)
+    {
+        foreach (var param in self.parameters)
+        {
+            if (param.type == type && param.nameHash == hash)
+                return true;
+        }
+        return false;
+    }
+}
+
